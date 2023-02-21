99dae65e68d99b0f78718d6c2eedc6198abc3f77
mihir.993 <mihir.993@gmail.com>
2023-02-21 22:41:17 +0100

    Initial commit.

---
+++ Wifi/serialMonitor.h
@@ -0,0 +1,11 @@
+
+void setup(void) {
+  // We'll send debugging information via the Serial monitor
+  Serial.begin(115200);   
+}
+
+void loop(void) {
+  Serial.print("Analog reading = ");
+  
+  delay(1000);
+}
