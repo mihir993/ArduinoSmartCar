7636d9b48d2dec3fc971c7718bb0b77d0711c137
mihir.993 <mihir.993@gmail.com>
2023-02-28 01:27:00 +0100

    Wifi connection works.

--- Wifi/Wifi.ino
+++ Wifi/Wifi.ino
@@ -1,9 +1,14 @@
+#include "config.h"
+#include "wifi.h"
+
+const char* ssid = WIFISSID;
+const char* pwd = WIFIPWD;
+
 void setup() {
   // put your setup code here, to run once:
-
+  connect_to_wifi(ssid, pwd);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
--- Wifi/wifi.h
+++ Wifi/wifi.h
@@ -4,21 +4,14 @@
 */
 
 #include <ESP8266WiFi.h>
-#include "config.h"
+// #include "config.h"
 
-#ifndef STASSID
-#define STASSID ssid
-#define STAPSK pwd
-#endif
+// #ifndef STASSID
+// #define STASSID ssid
+// #define STAPSK pwd
+// #endif
 
-const char* ssid = STASSID;
-
-const char* password = STAPSK;
-
-const char* host = "djxmmx.net";
-const uint16_t port = 17;
-
-void setup() {
+void connect_to_wifi(const char* wifi_name, const char* wifi_pwd){
   Serial.begin(115200);
 
   // We start by connecting to a WiFi network
@@ -26,13 +19,13 @@ void setup() {
   Serial.println();
   Serial.println();
   Serial.print("Connecting to ");
-  Serial.println(ssid);
+  Serial.println(wifi_name);
 
   /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
      would try to act as both a client and an access-point and could cause
      network-issues with your other WiFi-devices on your WiFi-network. */
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+  WiFi.begin(wifi_name, wifi_pwd);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -42,55 +35,5 @@ void setup() {
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
-void loop() {
-  static bool wait = false;
-
-  Serial.print("connecting to ");
-  Serial.print(host);
-  Serial.print(':');
-  Serial.println(port);
-
-  // Use WiFiClient class to create TCP connections
-  WiFiClient client;
-  if (!client.connect(host, port)) {
-    Serial.println("connection failed");
-    delay(5000);
-    return;
-  }
-
-  // This will send a string to the server
-  Serial.println("sending data to server");
-  if (client.connected()) { client.println("hello from ESP8266"); }
-
-  // wait for data to be available
-  unsigned long timeout = millis();
-  while (client.available() == 0) {
-    if (millis() - timeout > 5000) {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      delay(60000);
-      return;
-    }
-  }
-
-  // Read all the lines of the reply from server and print them to Serial
-  Serial.println("receiving from remote server");
-  // not testing 'client.connected()' since we do not need to send data here
-  while (client.available()) {
-    char ch = static_cast<char>(client.read());
-    Serial.print(ch);
-  }
-
-  // Close the connection
-  Serial.println();
-  Serial.println("closing connection");
-  client.stop();
-
-  if (wait) {
-    delay(300000);  // execute once every 5 minutes, don't flood remote service
-  }
-  wait = true;
+  Serial.println(WiFi.localIP());  
 }
